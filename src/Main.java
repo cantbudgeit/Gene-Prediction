15c7b686939ccf84e3c587029f271cecc90093f3
ianap <ian.appelgate@gmail.com>
2018-03-26 10:08:21 -0500

    added a file scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //this will be the main class of the program
 
 public class Main {
+	
+	public static void main(String[] args)
+	{
+		File file = new File("gene.txt");
+		
+		String gene = "";
+		try {
+			Scanner scan = new Scanner(file);
+			while(scan.hasNextLine())
+			{
+				gene = scan.next();
+			}
+			scan.close();
+		}
+		
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+			
+		
+	}
 
 }
